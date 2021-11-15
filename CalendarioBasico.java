01f9cdaee5cbba8b94bde7b703e8ee348b6f1d77
Pablo <pablovivasgarcia1@gmail.com>
2021-11-15 14:25:03 +0100

    0321.02 1-1-1

--- CalendarioBasico.java
+++ CalendarioBasico.java
@@ -8,7 +8,9 @@
 public class CalendarioBasico
 {
     // instance variables - replace the example below with your own
-    private int x;
+    private int dia;
+    private int mes;
+    private int año;
 
     /**
      * Constructor for objects of class CalendarioBasico
@@ -16,18 +18,8 @@ public class CalendarioBasico
     public CalendarioBasico()
     {
         // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+        dia = 1;
+        mes = 1;
+        año = 1;
     }
 }
